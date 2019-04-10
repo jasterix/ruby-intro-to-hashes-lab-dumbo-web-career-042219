9909dfcb3e2a0b54a66d4c4504c4f12973313803
Jasterix C <loune.calixte@gmail.com>
2019-04-10 18:46:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,12 @@ def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
 			pieces: 4,
-			names:{},
-			rent_in_dollars:{}
+			names:{
+			  
+			},
+			rent_in_dollars:{
+			  
+			}
 			}
 		}
 end
@@ -32,10 +36,18 @@ def monopoly_with_third_tier
 		railroads: {
 			pieces: 4,
 			names:{
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {},
+				:reading_railroad => {
+				  
+				},
+				:pennsylvania_railroad => {
+				  
+				},
+				:b_and_o_railroad => {
+				  
+				},
+				:shortline => {
+				  
+				},
 			},
 			rent_in_dollars:{
 				:one_piece_owned => 25,
